92dce056225c4146fcfd461ece2312b7bc9a8a6f
fikrimhammd <fikri.kdi60@gmail.com>
2026-01-09 14:23:52 +0700

    memperbarui codingan ContainerApp.kt

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -1,20 +1,20 @@
 package com.example.myfirebase.repositori
 
+import FirebaseRepositorySiswa
+import com.example.myfirebase.repositori.RepositorySiswa
 import android.app.Application
 
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
 
-class DefaultContainerApp : ContainerApp {
+class DefaultContainerApp: ContainerApp{
     override val repositorySiswa: RepositorySiswa by lazy {
         FirebaseRepositorySiswa()
     }
 }
-
-class AplikasiDataSiswa : Application() {
+class AplikasiDataSiswa: Application(){
     lateinit var container: ContainerApp
-
     override fun onCreate() {
         super.onCreate()
         this.container = DefaultContainerApp()
